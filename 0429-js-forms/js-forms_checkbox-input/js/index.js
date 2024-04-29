ff55363ab4cf6447f9d956c55e54c80f0a15181f
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-04-29 12:34:50 +0200

    add first and second challenge

---
+++ 0429-js-forms/js-forms_checkbox-input/js/index.js
@@ -0,0 +1,44 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const tosError = document.querySelector('[data-js="tos-error"]');
+const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
+
+function hideTosError() {
+  tosError.setAttribute("hidden", "");
+}
+
+function showTosError() {
+  tosError.removeAttribute("hidden");
+}
+
+function hideSuccess() {
+  success.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // --v-- write your code here --v--
+  if (!tosCheckbox.checked) {
+    return alert("Please agree to our terms of service.");
+  }
+  // --^-- write your code here --^--
+
+  // eslint-disable-next-line no-alert
+  showSuccess();
+  alert("Form submitted");
+});
