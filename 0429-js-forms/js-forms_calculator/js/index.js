d9b45231f5f8435784c4e9665cdc6977bfd25ac4
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-04-29 12:48:55 +0200

    add last challenge

--- 0429-js-forms/js-forms_calculator/js/index.js
+++ 0429-js-forms/js-forms_calculator/js/index.js
@@ -23,8 +23,19 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let result;
+  const A = Number(event.target.numberA.value);
+  const B = Number(event.target.numberB.value);
 
   // --v-- write your code here --v--
+  if (event.target.operator.value === "addition") {
+    result = add(A, B);
+  } else if (event.target.operator.value === "subtraction") {
+    result = subtract(A, B);
+  } else if (event.target.operator.value === "multiplication") {
+    result = multiply(A, B);
+  } else {
+    result = divide(A, B);
+  }
 
   // --^-- write your code here --^--
 
