c894192e85bc728d41997e012915fe5cefe779a5
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-04-30 11:37:12 +0200

    add an array for a random toast-message

--- 0430-js-create-element/js-createelement_toast-messages/js/index.js
+++ 0430-js-create-element/js-createelement_toast-messages/js/index.js
@@ -7,7 +7,18 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 addButton.addEventListener("click", () => {
   const newToast = document.createElement("li");
   newToast.classList.add("toast-container__message");
-  newToast.textContent = "A cheer for all of us! :-)";
+  // newToast.textContent = "A cheer for all of us! :-)";
+  //// trying things out ////
+  const toastMessages = [
+    "A cheer for all of us!",
+    "Happy coding!",
+    "Have a nice day.",
+    "What the array",
+    "Jippie :-)",
+  ];
+  const randomItem = Math.floor(Math.random() * toastMessages.length);
+  newToast.textContent = toastMessages[randomItem];
+  //////////////////////////////////
   toastContainer.append(newToast);
 });
 
