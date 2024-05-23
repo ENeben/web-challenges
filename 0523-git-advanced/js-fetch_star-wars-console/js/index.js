7ae2bf9b5e58401919a7bcb5ff6824db445806f8
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-05-23 17:08:06 +0200

    Save the current solution to try to compare the different versions

--- 0523-git-advanced/js-fetch_star-wars-console/js/index.js
+++ 0523-git-advanced/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results[2].eye_color);
+}
 
 fetchData();
