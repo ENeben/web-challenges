097854a61bae2b16c9d1280d9aee9bf45f507aad
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-05-29 12:03:57 +0200

    solve challenges for react basics

--- 0529-react-basics/react-basics_attributes/src/App.js
+++ 0529-react-basics/react-basics_attributes/src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <ArticleComponent />;
+}
+
+function ArticleComponent() {
+  return (
+    <article className="article">
+      <h2 className="article__title">I am the second headline.</h2>
+      <label htmlFor="input"></label>
+      <input id="input"></input>
+      <a className="article__link" href="https://react.dev/">
+        Discover React!
+      </a>
+    </article>
+  );
 }
