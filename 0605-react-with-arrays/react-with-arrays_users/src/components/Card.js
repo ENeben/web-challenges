42dab10aa4d32320fc46e9659f8db2ab54699ba9
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-05 12:20:36 +0200

    move ul outside the map-return

--- 0605-react-with-arrays/react-with-arrays_users/src/components/Card.js
+++ 0605-react-with-arrays/react-with-arrays_users/src/components/Card.js
@@ -5,14 +5,11 @@ function Card({ user }) {
   return (
     <article className="card">
       <h2>{user.name}</h2>
-      {user.roles.map((role) => {
-        return (
-          <ul key={role} className="card__taglist">
-            <Tag tag={role} />
-          </ul>
-        );
-      })}
-
+      <ul className="card__taglist">
+        {user.roles.map((role) => {
+          return <Tag key={role} tag={role} />;
+        })}
+      </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
       <Button buttonType="danger">invoice</Button>
--- 0605-react-with-arrays/react-with-arrays_users/src/components/Tag.js
+++ 0605-react-with-arrays/react-with-arrays_users/src/components/Tag.js
@@ -3,7 +3,5 @@ import "./Tag.css";
 export default function Tag({ tag }) {
   return (
     <li className={`tag ${tag === "admin" && "tag--highlight"}`}>{tag}</li>
-
-    // <li className={tag === "admin" ? "tag tag--highlight" : "tag"}>{tag}</li>
   );
 }
