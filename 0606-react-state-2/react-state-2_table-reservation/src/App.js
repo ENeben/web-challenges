9ab832c6618939ec24f59ae5a1e2d2770f5de395
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-06 13:11:20 +0200

    add incomplete  challenges

---
+++ 0606-react-state-2/react-state-2_table-reservation/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+  // function handleAddPeople() {
+  //   setPeople(people + 1);
+  // }
+  // function handleSubtractPeople() {
+  //   setPeople(people - 1);
+  // }
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter
+        people={people}
+        onAdd={() => {
+          setPeople(people + 1);
+        }}
+        onSubtract={() => {
+          setPeople(people - 1);
+        }}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
