3c14c90c1d21ca06c0aba0f9be46ae68289d533f
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-10 13:38:10 +0200

    add changes

--- 0710-backend-basics/backend-basics_greeting/server.js
+++ 0710-backend-basics/backend-basics_greeting/server.js
@@ -4,3 +4,12 @@ export const server = createServer((request, response) => {
   response.statusCode = 200;
   response.end("Hello, Elisabeth!");
 });
+
+//das ist das gleiche wie hier:
+
+export const anotherServer = createServer(callbackFunction);
+
+function callbackFunction(request, response) {
+  response.statusCode = 200;
+  response.end("Hello, Elisabeth!");
+}
