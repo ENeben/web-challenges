3c28678d63263713dbbbc0def37cc03aefb0d6ba
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-12 09:27:25 +0200

    add mutation to a reload button to the random page

--- 0711-backend-api-routes/backend-api-routes_products/pages/products/random.js
+++ 0711-backend-api-routes/backend-api-routes_products/pages/products/random.js
@@ -1,8 +1,13 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
 
 export default function RandomFishPage() {
+  const { mutate } = useSWRConfig();
   const { data: randomFish, isLoading } = useSWR("/api/products/random");
+
+  function reloadRandomFish() {
+    mutate("/api/products/random");
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -22,6 +27,7 @@ export default function RandomFishPage() {
         </li>
         <li>category: {randomFish.category}</li>
       </ul>
+      <button onClick={reloadRandomFish}>Get a new random fish</button>
       <Link href="/products">Back to all fishes</Link>
     </>
   );
